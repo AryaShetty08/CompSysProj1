1d93566b2e3d22937ed58f6d7df8f2b7d23643b4
AryaShetty08 <arya.shetty08@gmail.com>
2024-03-31 17:35:34 -0400

    Finished generation

--- populate.c
+++ populate.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <strings.h>
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -9,24 +10,84 @@
 #include <math.h>
 #include <time.h>
 
-void printRandoms(int lower, int upper, int count) {
-    for (int i = 0; i < count; i++) {
-        int num = (rand() %
-        (upper - lower + 1)) + lower;
-        printf("%d ", num);
+#define SIZE 10000
+#define NUM_NEGATIVE_KEYS 60
+
+//Generate random positive integers
+int generatePositiveInt() {
+    return rand() % 1000 + 1; // change number to decide what range
+}
+
+//Generate random negative integers
+int generateNegativeInt() {
+    return -1 * (rand() % NUM_NEGATIVE_KEYS + 1); //based on H generate keys  
+}
+
+//randomize integer order
+void shuffleNumbers(int *array, int n) {
+    if (n > 1) {
+        for (int i = 0; i < n-1; i++) {
+            int j = i + rand() / (RAND_MAX / (n-i) + 1);
+            int t = array[j];
+            array[j] = array[i];
+            array[i] = t;
+        }
     }
 }
 
-int main(int argc, char const *argv[]) {
-    FILE *fp1;
-    fp1 = fopen("integers.txt", "a+");
+//Input integers into text file
+void writeToFile(int numbers[], int size) {
+    FILE *fp1 = fopen("integers.txt", "w+");
+    if (fp1 == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
 
-    srand(time(NULL));
-    int r = rand();
-
-    printRandoms(-60, -1, 10);
+    for (int i = 0; i < size; i++) {
+        fprintf(fp1, "%d\n", numbers[i]);
+    }
 
     fclose(fp1);
+}
+
+//Calculate max of numbers
+int calculateMax(int numbers[], int size) {
+    int max = numbers[0];
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+
+//Calculate average
+float calculateAverage(int numbers[], int size) {
+    float sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += numbers[i];
+    }
+    return sum/size;
+}
+
+//main function
+int main(int argc, char const *argv[]) {
+    int numbers[SIZE];
+    srand(time(NULL)); //seed
+
+    for (int i = 0; i < SIZE - NUM_NEGATIVE_KEYS; i++) {
+        numbers[i] = generatePositiveInt();
+    }
+
+    for (int i = SIZE - NUM_NEGATIVE_KEYS; i < SIZE; i++) {
+        numbers[i] = generateNegativeInt();
+    }
+
+    shuffleNumbers(numbers, SIZE);
+    writeToFile(numbers, SIZE);
+
+    printf("MAX: %d\n", calculateMax(numbers, SIZE));
+    printf("AVG: %.2f\n", calculateAverage(numbers, SIZE));
 
     exit(EXIT_SUCCESS);
 }
