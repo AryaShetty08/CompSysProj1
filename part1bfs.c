312c56fe236240f482f131308a64f2c26748788f
Damon Lin <damonlin8@gmail.com>
2024-04-04 03:56:49 -0400

    fatal bug not fixed yet

--- part1bfs.c
+++ part1bfs.c
@@ -39,28 +39,7 @@ int main(int argc, char const *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // ask the user how many children each fork should create
-    int maxChildren;
-    printf("How many children should each fork create? (2,3) \n");
-    scanf("%d", &maxChildren);
-
-    // checks constraints on maxChildren
-    if (maxChildren != 2 && maxChildren != 3) {
-        perror("Invalid value for maxChildren");
-        exit(EXIT_FAILURE);
-        return -1;
-    }
-
-    // some initializations
-    // pid_t pid[PN];
-    int rootNode = getpid();
     int keys[L];
-    // int foundKeysIndices[L];
-    // int foundKeys[L];
-    int start = 0, end = L, oldEnd;
-    // time variables
-    clock_t startTime = clock(), endTime;
-    double timeSpent = 0.0;
 
     // reads the file and stores the values in the keys array
     FILE *fp = fopen("integers.txt", "r");
@@ -74,20 +53,40 @@ int main(int argc, char const *argv[]) {
     }
     fclose(fp);
 
+    // ask the user how many children each fork should create
+    int maxChildren;
+    printf("How many children should each fork create? (2 or 3) \n");
+    scanf("%d", &maxChildren);
+
+    // checks constraints on maxChildren
+    if (maxChildren != 2 && maxChildren != 3) {
+        perror("Invalid value for maxChildren");
+        return -1;
+    }
+
+    // some initializations
+    // pid_t pid[PN];
+    int rootNode = getpid();
+    printf("Root node: %d\n", rootNode);
+    // int foundKeysIndices[L];
+    // int foundKeys[L];
+    int start = 0, end = L, oldEnd;
+    // time variables
+    clock_t startTime = clock(), endTime;
+    double timeSpent = 0.0;
+
     // initalize array with pid of children who found the key, their return argument code, and the index of the key
     int returnH[H*3];
     int startH = 0;
     // initialize counter for printing
-    int returnArg = 1;
-    int returnCode;
+    int returnArg = 1, foundArg;
     int fd[2*PN];
     // tracks the parent pipe
     int parentPipe;
     // tracks the number of children in BFS
     int bfsChildren[3] = {-1, -1, -1};
     int pid;
-    pid_t childTracker;
-    int childCounter = -1;
+    pid_t canMakeChildren;
 
     // loops through and creates PN number of children
     for (int i = 0; i < floor(log2(PN)); i++) {
@@ -101,13 +100,13 @@ int main(int argc, char const *argv[]) {
             }
         }
 
-        childTracker = getpid();
+        canMakeChildren = getpid();
         int increment = ceil((end-start) / maxChildren);
         oldEnd = end;
         end = start;
 
         for (int j = 0; j < maxChildren; j++) {
-            if (childTracker == getpid() && returnArg < PN) {
+            if (canMakeChildren == getpid() && returnArg < PN) {
                 for (int k = 0; k < maxChildren; k++) {
                     if (bfsChildren[k] == -1) {
                         bfsChildren[k] = returnArg - 1;
@@ -119,22 +118,30 @@ int main(int argc, char const *argv[]) {
                 pid = fork();
 
                 if(j != 0) {
-                    start = start + increment;
+                    start += increment;
                 }
-                end = end + increment;
+                end += increment;
 
-                if (L - end < 5) {
+                // checks if L - end is less than some value to prevent overrunning the array
+                if (L - end < 4) {
                     end = L;
                 }
                 returnArg += 1;
-                returnCode = returnArg;
+                foundArg = returnArg;
             }
         }
 
+        printf("I am process %d and the root node is %d\n", getpid(), rootNode);
+
+        // printf("My pid is %d\n", getpid());
+        // if (getpid() == rootNode) {
+        //     printf("brug1");
+        // }
+
         if (pid == -1) {
             perror("Failed to fork process");
-            // exit(EXIT_FAILURE);
-        } else if (pid == 0) {          // child process
+        } else if (pid == 0) {
+            // child process
             for (int j = 0; j < maxChildren; j++) {
                 if (bfsChildren[j] != -1) {
                     parentPipe = bfsChildren[j];
@@ -142,9 +149,16 @@ int main(int argc, char const *argv[]) {
                 }
             }
 
+            if (getpid() == rootNode) {
+                printf("brug2");
+            }
+
             printf("Hi I'm process %d with return arg %d and my parent is %d.\n", getpid(), returnArg, getppid());
 
+            // sets pid to the current process id, which ensures that the child process can be tracked
             pid = getpid();
+
+            // checks if the current process is the last process
             if (i == floor(log2(PN)) - 1) {
                 int max = 0, count = 0;
                 int64_t avg = 0;
@@ -156,7 +170,7 @@ int main(int argc, char const *argv[]) {
                     if (keys[j] < 0) {
                         returnH[startH] = getpid();
                         returnH[startH+1] = j;
-                        returnH[startH+2] = returnCode;
+                        returnH[startH+2] = foundArg;
                         startH += 3;
                     }
                 }
@@ -171,14 +185,20 @@ int main(int argc, char const *argv[]) {
                 write(fd[2*parentPipe+1], &startH, sizeof(startH));
                 close(fd[2*parentPipe+1]);
 
+                // printf("test1");
+                // printf("My pid is %d\n", getpid());
+
                 exit(0);
             }
         } else {
             // parent process
+            if (getpid() == rootNode) {
+                printf("brug4");
+            }
 
             bool hasChild = false, tracked = false;
-            int64_t avg = 0;
-            int childCount = 0, max = 0, count = 0, tempStartH = 0, tempMax = -1, tempAvg = -1, tempCount = -1;
+            int64_t avg = 0, tempAvg = -1;
+            int childCount = 0, max = 0, count = 0, tempStartH = 0, tempMax = -1, tempCount = -1;
             int tempReturnH[H*3];
             
             for (int j = 0; j < maxChildren; j++) {
@@ -205,7 +225,7 @@ int main(int argc, char const *argv[]) {
                     if (keys[j] < 0) {
                         returnH[startH] = getpid();
                         returnH[startH+1] = j;
-                        returnH[startH+2] = returnCode;
+                        returnH[startH+2] = foundArg;
                         startH += 3;
                     }
                 }
@@ -220,7 +240,7 @@ int main(int argc, char const *argv[]) {
                     if (keys[j] < 0) {
                         returnH[startH] = getpid();
                         returnH[startH+1] = j;
-                        returnH[startH+2] = returnCode;
+                        returnH[startH+2] = foundArg;
                         startH += 3;
                     }
                 }
@@ -229,10 +249,11 @@ int main(int argc, char const *argv[]) {
 
                 for (int j = 0; j < childCount; j++) {
                     read(fd[2*bfsChildren[j]], &tempMax, sizeof(tempMax));
-                    read(fd[2*bfsChildren[j]], &tempAvg, sizeof(tempAvg));
+                    read(fd[2*bfsChildren[j]], &tempAvg, sizeof(int64_t));
                     read(fd[2*bfsChildren[j]], &tempCount, sizeof(tempCount));
-                    read(fd[2*bfsChildren[j]], &tempReturnH, sizeof(tempReturnH));
+                    read(fd[2*bfsChildren[j]], &tempReturnH, sizeof(int) * (H*3));
                     read(fd[2*bfsChildren[j]], &tempStartH, sizeof(tempStartH));
+                    // printf("test2");
 
                     if (tempMax >= max) {
                         max = tempMax;
@@ -250,10 +271,11 @@ int main(int argc, char const *argv[]) {
                 for (int j = 0; j < childCount; j++) {
                     if (j != 0) {
                         read(fd[2*bfsChildren[j]], &tempMax, sizeof(tempMax));
-                        read(fd[2*bfsChildren[j]], &tempAvg, sizeof(tempAvg));
+                        read(fd[2*bfsChildren[j]], &tempAvg, sizeof(int64_t));
                         read(fd[2*bfsChildren[j]], &tempCount, sizeof(tempCount));
-                        read(fd[2*bfsChildren[j]], &tempReturnH, sizeof(tempReturnH));
+                        read(fd[2*bfsChildren[j]], &tempReturnH, sizeof(int) * (H*3));
                         read(fd[2*bfsChildren[j]], &tempStartH, sizeof(tempStartH));
+                        // printf("test3");
 
                         if (tempMax >= max) {
                             max = tempMax;
@@ -272,18 +294,22 @@ int main(int argc, char const *argv[]) {
                         read(fd[2*bfsChildren[j]], &count, sizeof(count));
                         read(fd[2*bfsChildren[j]], &returnH, sizeof(int) * (H*3));
                         read(fd[2*bfsChildren[j]], &startH, sizeof(startH));
+                        // printf("test4");
                     }
                 }
             }
 
+            // printf("My pid is %d\n", getpid());
+
             if (rootNode != getpid()) {
-                close(fd[2*PN]);
+                // close(fd[2*PN]);
                 write(fd[2*PN+1], &max, sizeof(max));
                 write(fd[2*PN+1], &avg, sizeof(avg));
                 write(fd[2*PN+1], &count, sizeof(count));
                 write(fd[2*PN+1], &returnH, sizeof(int) * (H*3));
                 write(fd[2*PN+1], &startH, sizeof(startH));
-                close(fd[2*PN+1]);
+                // printf("test5");
+                // close(fd[2*PN+1]);
             } else {
                 printf("brug");
                 wait(NULL);
@@ -296,11 +322,16 @@ int main(int argc, char const *argv[]) {
                 endTime = clock();
                 timeSpent = (double) (endTime - startTime) / CLOCKS_PER_SEC;
                 printf("\nThe program completed in %f seconds \n", timeSpent);
-                exit(EXIT_SUCCESS);
+                // printf("My pid is %d\n", getpid());
+                exit(0);
+            }
+
+            if (getpid() == rootNode) {
+                printf("brug3");
             }
 
             wait(NULL);
-            exit(EXIT_SUCCESS);
+            exit(0);
         }
     }
 
